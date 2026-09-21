9d81191bbb05d52b78d9bf4d46fa4ad929a48daf
Mahliyo <gasandigital13@gmail.com>
2026-09-21 16:28:42 +0500

    Javascript asoslari

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //1-masala Bigginer
-let R = 2;
-const PI = 3.14;
-let L = 2 * PI * 2;
-let S = PI * Math.pow(R, 2);
-console.log("Uzunligi =" ,L, "Yuzasi = " ,S,);
+// let R = 2;
+// const PI = 3.14;
+// let L = 2 * PI * 2;
+// let S = PI * Math.pow(R, 2);
+// console.log("Uzunligi =" ,L, "Yuzasi = " ,S,);
