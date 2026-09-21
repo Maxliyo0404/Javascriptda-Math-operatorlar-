fdc2688e5127508338ec61826916f91f61092e32
Mahliyo <gasandigital13@gmail.com>
2026-09-21 17:08:25 +0500

    Javascript asoslari va masalalar to'plami

--- main.js
+++ main.js
@@ -22,6 +22,7 @@
 let X = 4;
 let A = 1600;
 let Y = 2;
-let bir = A / X;
-let result = bir * Y;
-console.log("1kg konfet narxi" ,bir);
+let birkg = A / X;
+let result = birkg * Y;
+console.log("1kg konfet narxi" ,birkg);
+console.log(Y + " 1kg konfet narxi:", result);
