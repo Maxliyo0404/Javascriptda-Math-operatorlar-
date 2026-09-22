5c295b4326834ed77b88997b654a0bc94012f93f
Mahliyo <gasandigital13@gmail.com>
2026-09-22 20:15:35 +0500

    Qudrat aka masalalari

--- main.js
+++ main.js
@@ -26,3 +26,83 @@
 // let result = birkg * Y;
 // console.log("1kg konfet narxi" ,birkg);
 // console.log(Y + " 1kg konfet narxi:", result);
+
+// // 5-masala Bigginer
+  
+// let A = 5;
+// let B = 4;
+// let a = B;
+// let b = A;
+// console.log("a sonininy qiymati" ,B,);
+// console.log("b sonininy qiymati" ,A,);
+
+// 1-masala Integer
+
+// let number = 24;
+// let a = Math.floor(number / 10);
+// let b = Math.floor(number % 10);
+// let result = a + b;
+// console.log(result);
+
+
+// 2 -masala Integer
+// let number = 25;
+
+// let a = Math.floor(number / 10);
+// let b = number % 10;
+// let result = (b * 10) + a;
+
+// console.log(result);
+
+// 3 -masala Integer
+
+// let son = 862;
+// let a = Math.floor(son / 100);
+// let b = Math.floor(son / 10 % 10);
+// let c = Math.floor(son % 10);
+// console.log("yuzlar xonsi =" ,a, );
+// console.log("o'nlar xonsi =" ,b, );
+// console.log("birlar xonsi =" ,c, );
+
+// 4-masala Integer
+
+// let son = 1239;
+// let a = Math.floor(son / 100 % 10);
+// console.log(a);
+
+// 5-masala Integer Tushunmadim
+
+    // Uyga vazifa
+    //1. Begin
+    // let a = 4;
+    // let b = 5;
+    // let S = a * b;
+    // let P = 2 * (a + b);
+    // console.log("Yuzasi =" ,S,);
+    // console.log("Peremeterini =" ,P,);
+    
+    
+  //2. Begin
+//   let d = 8;
+//   const PI = 3.14;
+//   let L = PI * d;
+//   let r = d / 2;
+//   let S = PI * Math.pow(r,2) ;
+//   console.log("Uzunligi = ",L,);
+//   console.log("Yuzasi = ",S,);
+  
+//3. Begin
+//  let a = 4;
+//  let V = Math.pow(a, 3);
+//  let S = 6 *  Math.pow(a, 2); 
+//  console.log("hajmi =" ,V,);
+//  console.log("sirti =" ,S,);
+ 
+//4. Begin
+// let a = 4;
+// let b = 2;
+// let c = 3;
+// let V = a * b * c;
+// let S = 2 * (a * b + b * c + a * c);
+//  console.log("hajmi =" ,V,);
+//  console.log("sirti =" ,S,);
