0d9a716831dc3f9093c210497c043d767402e53a
Mahliyo <gasandigital13@gmail.com>
2026-09-24 20:34:01 +0500

    Javaskript mantiqiy operatorlar booleanlar

--- main.js
+++ main.js
@@ -302,7 +302,8 @@
 // }
 let a = +prompt("a sonini kiriting");
 let b = +prompt("b sonini kiriting");
-if (a > 2 && b <= 3) {
+let c = +prompt("c sonini kiriting");
+if (a <= b <= 3) {
   alert("true")
 }else{
   alert("false")
