ab437669a2f3801a56aac893fa6e1f1ea8d5ab7c
Mahliyo <gasandigital13@gmail.com>
2026-09-24 21:19:14 +0500

    Javaskript mantiqiy operatorlar booleanlar

--- main.js
+++ main.js
@@ -342,3 +342,13 @@
 //    ("false")
 //   }
  
+// //Boolean8.
+  let a = +prompt("a sonini kiriting");
+  let b = +prompt("b sonini kiriting");
+  let c = +prompt("c sonini kiriting");
+  if (a === b || a === c || b === c) {
+       alert("true")
+  }else{
+   ("false")
+  }
+  
